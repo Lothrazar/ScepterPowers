bb03e6c05db286fc289da6b6216d67e1c2608e71
sam bassett <samson.bassett@gmail.com>
2015-12-09 22:12:40 -0800

    new empty mod project

---
+++ src/main/java/com/lothrazar/scepterpowers/ModScepterPowers.java
@@ -0,0 +1,19 @@
+package com.lothrazar.scepterpowers;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = ModScepterPowers.MODID, useMetadata=true)
+public class ModScepterPowers
+{
+    public static final String MODID = "scepterpowers";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
