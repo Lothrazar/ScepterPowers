b6a1fc87a47aa04985ca194a7c891cc6b844a500
sam bassett <samson.bassett@gmail.com>
2015-12-10 20:42:45 -0800

    both events for chestsack

--- src/main/java/com/lothrazar/scepterpowers/item/ItemChestSack.java
+++ src/main/java/com/lothrazar/scepterpowers/item/ItemChestSack.java
@@ -5,12 +5,14 @@ import com.lothrazar.scepterpowers.ModScepterPowers;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntityChest;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;  
 import org.apache.logging.log4j.Level;
 
@@ -26,7 +28,34 @@ public class ItemChestSack extends Item
 		this.setMaxStackSize(1);
 		//imported from my old mod https://github.com/PrinceOfAmber/SamsPowerups/blob/b02f6b4243993eb301f4aa2b39984838adf482c1/src/main/java/com/lothrazar/samscontent/item/ItemChestSack.java
 	}
-   
+	
+	@Override
+	public boolean onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ)
+    {
+		
+		if(worldIn.getTileEntity(pos) != null && worldIn.getTileEntity(pos) instanceof IInventory){
+
+			
+			
+		}
+		else{
+			BlockPos offset = pos.offset(side);
+
+			//TODO : if you het existing IInventory then sort into it
+			
+			if(worldIn.isAirBlock(offset) == false){return false;}
+			
+			worldIn.setBlockState(offset, Blocks.chest.getDefaultState());
+			
+			
+			sortFromSackToChestEntity((TileEntityChest)worldIn.getTileEntity(offset),stack,offset);
+			
+			playerIn.inventory.decrStackSize(playerIn.inventory.currentItem, 1);
+		}
+		
+		return false;	
+    }
+	
 	@Override
 	public void addInformation(ItemStack itemStack, EntityPlayer player, List list, boolean par4) 
 	{
@@ -43,7 +72,7 @@ public class ItemChestSack extends Item
         list.add("Stacks: " + EnumChatFormatting.GREEN +stacks);          
 	}   
 	  
-	public static void sortFromSackToChestEntity(TileEntityChest chest, ItemStack held, BlockPos pos)
+	public void sortFromSackToChestEntity(IInventory chest, ItemStack held, BlockPos pos)
   	{
 		if(held.getTagCompound() == null) {held.setTagCompound(new NBTTagCompound());}
 	  
@@ -56,7 +85,7 @@ public class ItemChestSack extends Item
   		int totalItemsMoved = 0; 
   		int totalSlotsFreed = 0;
   		
-  		boolean debug = false;
+  		//boolean debug = false;
   	  	
 		ItemStack chestItem;
 		ItemStack invItem;
@@ -64,19 +93,18 @@ public class ItemChestSack extends Item
 		int toDeposit;
 		int chestMax;
 		  
-		int START_CHEST = 0;
-		int START_INV = 9;//because we are ignoring the item hotbar, we skip the first row this way
+		//int START_CHEST = 0;
+		//int START_INV = 9;//because we are ignoring the item hotbar, we skip the first row this way
 		//player inventory and the small chest have the same dimensions 
-		int size = 3*9;
-		int END_CHEST =  START_CHEST + size;
-		int END_INV = START_INV + size;
+		//int size = 3*9;
+ 
 
 		int item;
 		int meta;
 		int qty;
  
 		//inventory and chest has 9 rows by 3 columns, never changes. same as 64 max stack size
-		for(int islotChest = START_CHEST; islotChest < END_CHEST; islotChest++)
+		for(int islotChest = 0; islotChest < chest.getSizeInventory(); islotChest++)
 		{
 			chestItem = chest.getStackInSlot(islotChest);
 		
